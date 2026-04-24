ad21b4d8158eea3b455c217de7daef2a1cbbb6ad
Pratishtha103 <11f23pratishtha@gmail.com>
2026-04-24 14:36:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
